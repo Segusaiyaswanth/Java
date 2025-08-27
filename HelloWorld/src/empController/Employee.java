b86c91c34945db7b90ed59c9dca099473624e7a9
yaswa <yaswa@192.168.1.52>
2025-08-27 19:15:42 +0530

    getname added

--- HelloWorld/src/empController/Employee.java
+++ HelloWorld/src/empController/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(new Employee().getName());
 	}
 	
 	public String getName() {
