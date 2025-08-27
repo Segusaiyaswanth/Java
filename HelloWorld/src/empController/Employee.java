d3560403624654d99094f80259cf3d861a016f94
yaswa <yaswa@192.168.1.52>
2025-08-27 18:40:25 +0530

    Revert "two metod removed in master"
    This reverts commit 2625cfd7464306cbd4315ca00ea695b8e6fbb865.

--- HelloWorld/src/empController/Employee.java
+++ HelloWorld/src/empController/Employee.java
@@ -7,6 +7,12 @@ public class Employee {
 
 	}
 	
+	public String getName() {
+		return "Thirumalesh";
+	}
+	public String getDescription() {
+		return "kakarakaya";
+	}
 	public void Hello() {
 		System.out.println("hello thiru");
 	}
